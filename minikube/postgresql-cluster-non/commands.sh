2566b71afcad68147a17c443a2bc0d69dfd335c8
Jake Willmsen <jdwillmsen@gmail.com>
2024-05-12 16:51:51 -0500

    docs(minikube): add commands for db ui login for superuser

--- minikube/postgresql-cluster-non/commands.sh
+++ minikube/postgresql-cluster-non/commands.sh
@@ -3,3 +3,8 @@ kubectl get secret -n database postgresql-cluster-non-app -o jsonpath='{.data.pa
 # Default username: app
 # Default database: app
 # Service: postgresql-cluster-non-rw.database.svc
+# Get superuser account password
+kubectl get secret -n database postgresql-cluster-non-superuser -o jsonpath='{.data.password}' | base64 --decode
+# Default username: postgres
+# Default database: * - Can leave empty
+# Service: postgresql-cluster-non-rw.database.svc
