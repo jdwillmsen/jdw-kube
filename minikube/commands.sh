00694be24db0d4f47cb3dfc69acf89e697f3c34f
Jake Willmsen <jdwillmsen@gmail.com>
2024-05-11 21:44:41 -0500

    feat: add minikube configs + ingress examples

--- minikube/commands.sh
+++ minikube/commands.sh
@@ -1,8 +1,27 @@
 # Start Minikube
 minikube start \
+  --driver=hyperv \
   --profile=jdw-cluster \
   --addons=default-storageclass,ingress,ingress-dns \
   --nodes=5
+# --driver (options) | docker, ssh, hyperv
+
+# Status Minikube
+minikube status \
+  --profile=jdw-cluster
+
+# Restart Minikube
+minikube start \
+  --profile=jdw-cluster
+
+# Enable Minikube Add Ons
+minikube --profile=jdw-cluster addons enable ingress
+minikube --profile=jdw-cluster addons enable ingress-dns
+minikube --profile=jdw-cluster addons enable default-storageclass
+
+# Powershell admin
+Add-DnsClientNrptRule -Namespace ".jdw.com" -NameServers "$(minikube -p jdw-cluster ip)"
+Get-DnsClientNrptRule | Where-Object {$_.Namespace -eq '.jdw.com'} | Remove-DnsClientNrptRule -Force; Add-DnsClientNrptRule -Namespace ".test" -NameServers "$(minikube -p jdw-cluster ip)"
 
 kubectl apply -f argocd/argocd-namespace.yaml && \
 kubectl apply -f argocd/argocd.yaml -n argocd && \
