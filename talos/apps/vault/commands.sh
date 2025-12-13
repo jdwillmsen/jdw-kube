371d449199c0387ea1ba7c9cef8127d5db976255
Jake Willmsen <42048994+jdwillmsen@users.noreply.github.com>
2025-12-13 00:16:12 -0600

    feat(talos): add secrets management for porkbun

--- talos/apps/vault/commands.sh
+++ talos/apps/vault/commands.sh
@@ -6,3 +6,11 @@ metadata:
 data:
   token: token_here
 type: Opaque
+
+kubectl create secret generic vault \
+  --from-literal=token=<VAULT_TOKEN> \
+  -n vault
+
+kubectl create secret generic vault-token \
+  -n external-secrets \
+  --from-literal=token=s.xxxxxxxxx
