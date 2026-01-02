db5241067a7f4ee5b83bc8c3f4e14ddfa78c0834
Jake Willmsen <42048994+jdwillmsen@users.noreply.github.com>
2026-01-01 20:50:14 -0600

    feat(talos): update db schemas config

--- talos/apps/postgresql-cluster-non/commands.sh
+++ talos/apps/postgresql-cluster-non/commands.sh
@@ -12,3 +12,29 @@ kubectl get secret -n database postgresql-cluster-non-superuser -o jsonpath='{.d
 # Create databases
 CREATE DATABASE jdw_non;
 CREATE DATABASE dbtech_non;
+
+# Setup roles - dbtech_non
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app') THEN
+      CREATE ROLE app;
+    END IF;
+  END
+$$;
+
+GRANT USAGE ON SCHEMA public TO app;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app;
+
+# Setup roles - jdw_non
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app') THEN
+      CREATE ROLE app;
+    END IF;
+  END
+$$;
+
+GRANT USAGE ON SCHEMA auth TO app;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA auth TO app;
+ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app;
--- talos/apps/postgresql-cluster-prd/commands.sh
+++ talos/apps/postgresql-cluster-prd/commands.sh
@@ -9,6 +9,32 @@ kubectl get secret -n database postgresql-cluster-prd-superuser -o jsonpath='{.d
 # Default database: * - Can leave empty
 # Service: postgresql-cluster-prd-rw.database.svc
 
-# Create jdw database
+# Create databases
 CREATE DATABASE jdw_prd;
 CREATE DATABASE dbtech_prd;
+
+# Setup roles - dbtech_prd
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app') THEN
+      CREATE ROLE app;
+    END IF;
+  END
+$$;
+
+GRANT USAGE ON SCHEMA public TO app;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app;
+
+# Setup roles - jdw_prd
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app') THEN
+      CREATE ROLE app;
+    END IF;
+  END
+$$;
+
+GRANT USAGE ON SCHEMA auth TO app;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA auth TO app;
+ALTER DEFAULT PRIVILEGES IN SCHEMA auth GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app;
